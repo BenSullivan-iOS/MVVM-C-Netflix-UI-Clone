10ef86cdf9e1384854a2776ae2527755052e9c7a
Ben Sullivan <benskill@gmail.com>
2017-07-18 08:45:04 +0100

    Initial Commit

---
+++ NetflixMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetflixMVVM
+//
+//  Created by Ben Sullivan on 18/07/2017.
+//  Copyright © 2017 Ben Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
